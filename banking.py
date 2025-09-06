a74576635aaf46863a7cfcf9560dedf0067b073c
Dev-SIMONONI <bankislimbo18@gmail.com>
2025-09-06 09:46:11 +0100

    added messaging

--- banking.py
+++ banking.py
@@ -1,14 +1,9 @@
-'''
-1:Create account(name,account_num,balance,)generate random account number, if user have promo bala +2000
-'''
 import random
 class BankAccount:
     promo_prize = 2000
 
-    
-        
 
-    def __init__(self, name, balance, has_promo=False, isAdmin=False, isfrezze=False, message_type="SMS"):
+    def __init__(self, name, balance, has_promo=False, isAdmin=False, isfrezze=False, messaging=False, message_type="SMS"):
         if has_promo == True:
             balance += self.promo_prize
         self.fullname=name
@@ -16,6 +11,18 @@ class BankAccount:
         self.total_bal = balance
         self.Admin = isAdmin
         self.frozen= isfrezze
+        self.inbox_type = [message_type]
+        self.ismessage = messaging
+
+
+
+    def notify(self,trans_type,amount):
+        if self.ismessage == True:
+            for choice in self.inbox_type:
+                if choice == "SMS":
+                    return f"SMS: {trans_type} Alert of {amount} from {self.fullname}: {self.account_nums} Thank you for banking with us"
+                if choice == "E-MAIL":
+                    return f"E-MAIL {trans_type} \n Alert of {amount} from account details: {self.fullname}: {self.account_nums} \n if you did not approve this message send us a message to freeze this account \n Thank you for banking with us" 
 
 
     def frezze(self):
@@ -26,18 +33,13 @@ class BankAccount:
             return "You do not access to frezze this account"
 
 
-
-
-
-
-
-
     def deposit(self, amount):
         if self.frozen:
             return self.frezze()
         else:
             if amount > 0:
                 self.total_bal += amount
+                print(self.notify("CREDIT", amount))
                 return self.total_bal
             else:
                 return "Invalid amount"
@@ -46,6 +48,7 @@ class BankAccount:
     def withdraw(self,amount):
         if amount <= self.total_bal:
             self.total_bal -= amount
+            print(self.notify("DEBIT", amount))
             return"Withdraw sucessful"
             
         else:
@@ -56,7 +59,9 @@ class BankAccount:
         if amount <= self.total_bal:
             self.withdraw(amount)
             if TF == "Withdraw sucessful":
+                print(self.notify("DEBIT", amount))
                 recipent.deposit(amount)
+                rprint(recipent.notify("CREDIT", amount))
                 return "transfer sucessful"
             else:
                 return "withdrawal not sucessful"
@@ -64,27 +69,16 @@ class BankAccount:
         else:
             return "Insuffient funds"
 
-
-    
-
-
         
-
-    
-
-
-
-simon = BankAccount("stephen", 1000, isAdmin = True)
+simon = BankAccount("stephen", 1000, isAdmin = True, messaging = True, message_type="SMS")
 klaus = BankAccount("klaus", 4000)
 
-
-print(simon.deposit(200))
-print(simon.frezze())
 print(simon.deposit(200))
 
+print(simon.withdraw(200))
 
 
-print(klaus.deposit(100))
+
 
 
 
